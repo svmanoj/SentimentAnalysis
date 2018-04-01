d3bb32494a74acc11b12172e5de264055c43dfb7
MANOJ_SV <svmanoj30@gmail.com>
2018-04-01 21:53:10 +0530

    changes in complexity

--- mainnn.py
+++ mainnn.py
@@ -3,11 +3,8 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from nltk import tokenize
 #nltk.download('punkt')
 #nltk.download('vader_lexicon')
-def getSen(a):
-    sentence = a
-    print(sentence)
-    sid = SentimentIntensityAnalyzer()
-    ss = sid.polarity_scores(sentence)
-    """for k in sorted(ss):
-        print('{0}: {1}, '.format(k, ss[k]), '')"""
-    return ss['compound']
+sentence = input()
+sid = SentimentIntensityAnalyzer()
+ss = sid.polarity_scores(sentence)
+print(ss['compound']) 
+
